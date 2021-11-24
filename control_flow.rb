4b3cbe7e5f5e3e2340049863b02d476592fff75d
Michael Neis <michael.neis12@gmail.com>
2021-11-23 21:31:57 -0600

    commit tree ff0c391de7fe121f661da7b1db5ab94e115d45e5

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,48 @@
+require "pry"
+
 def admin_login(username, password)
-  # your code here
+  return "Access denied" unless username.downcase == "admin" && password == "12345"
+  "Access granted"
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  case
+  when temperature < 40
+    "It's brisk out there!"
+  when temperature < 65
+    "It's a little chilly out there!"
+  when temperature < 85
+    "It's perfect out there!"
+  else
+    "It's too dang hot out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  case 
+  when (num%3) == 0 && (num%5) == 0
+    "FizzBuzz"
+  when (num%3) == 0
+    "Fizz"
+  when (num%5) == 0
+    "Buzz"
+  else
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case
+  when operation == "+"
+    num1 + num2
+  when operation == "-"
+    num1 - num2
+  when operation == "/"
+    num1 / num2
+  when operation == "*"
+    num1 * num2
+  else
+    puts "Invalid operation!"
+  end
 end
 
